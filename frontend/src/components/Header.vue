130c1b295eeded3c22dd9ac60f594aa52459e4ef
luxxv <46924122+luxxv@users.noreply.github.com>
2019-05-05 17:32:12 +0200

    Delete Header.vue

--- frontend/src/components/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-toolbar>
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <img src="./pic/Image2.gif" width="180" height="70">
-    <v-spacer></v-spacer>
-    <span><a style="font-weight:bold">PSEUDO</a></span>
-    <v-btn color="white" fab></v-btn>
-  </v-toolbar>
-</template>
